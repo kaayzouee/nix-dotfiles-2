b7581271b652f576a61ad29a5b235f0044f146b7
Kay <kaayzouee@gmail.com>
2026-09-12 13:40:40 +0700

    feat: harderned security for KVM

--- modules/system/virtualization.nix
+++ modules/system/virtualization.nix
@@ -1,4 +1,22 @@
 {
-  virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd = {
+    enable = true;
+
+    # Do not run the QEMU process as root.
+    qemu.runAsRoot = false;
+
+    # Re-enable libvirt's QEMU isolation mechanisms.
+    qemu.verbatimConfig = ''
+      namespaces = [ "mount" ]
+      seccomp_sandbox = 1
+    '';
+
+    # Never automatically resurrect an analysis VM after reboot.
+    onBoot = "ignore";
+  };
+
   programs.virt-manager.enable = true;
+
+  # Useful additional MAC confinement layer on Linux.
+  security.apparmor.enable = true;
 }
